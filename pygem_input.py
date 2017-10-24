d18378b378e33909cfaeff25a2125ddeb8f5420f
drounce <david.r.rounce@gmail.com>
2017-10-24 11:33:30 -0800

    working on finalizing output

--- pygem_input.py
+++ pygem_input.py
@@ -255,6 +255,10 @@ option_temp2bins = 1
 # Option 2 (no other options at this moment)
 option_melt_model = 1
 # Option 1 (default) DDF
+option_geometrychange = 1
+# Option 1 (default) - Geometry change computed based on Huss and Hock (2015),
+#                      i.e., volume gain/loss redistributed over the glacier
+#                      using empirical normalized ice thickness change curves
 
 #------- INPUT FOR STEP FOUR -------------------------------------------------
 # STEP FIVE: Output
