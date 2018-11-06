84bd56a424f6021e52cb044636fa454c434f067e
drounce <david.r.rounce@gmail.com>
2018-11-06 11:22:39 -0500

    fixed bug in spc_run_sim_gcms by trimming white space associated with gcm names

--- spc_run_simulation_gcm.sh
+++ spc_run_simulation_gcm.sh
@@ -8,8 +8,8 @@ echo num_nodes: $SLURM_JOB_NUM_NODES nodes: $SLURM_JOB_NODELIST
 echo num_tasks: $SLURM_NTASKS tasks_node: $SLURM_NTASKS_PER_NODE
 
 GCM_NAMES_FP="../Climate_data/cmip5/"
-#GCM_NAMES_FN="gcm_rcp26_filenames_glaciermip.txt"
-GCM_NAMES_FN="gcm_rcp26_filenames_nompiesmlr.txt"
+GCM_NAMES_FN="gcm_rcp26_filenames_glaciermip.txt"
+#GCM_NAMES_FN="gcm_rcp26_filenames_nompiesmlr.txt"
 #GCM_NAMES_FN="gcm_rcp26_filenames_single.txt"
 # determine gcm names and rcp scenario
 GCM_NAMES_LST="$(< $GCM_NAMES_FP$GCM_NAMES_FN)"
@@ -21,9 +21,11 @@ source activate pygem_hpc
 
 # region
 REGNO=$(python pygem_input.py)
-echo -e "Region: $REGNO\n"
+echo -e "\nRegion: $REGNO\n"
+echo -e "GCMs:\n$GCM_NAMES_LST\n"
 # region batch string
 rgi_batch_str="R${REGNO}_rgi_glac_number_batch"
+
 
 # delete previous rgi_glac_number batch filenames
 find -name '${rgi_batch_str}_*' -exec rm {} \;
@@ -39,6 +41,7 @@ list_rgi_fns=($rgi_fns)
 echo first_batch:${list_rgi_fns[0]}
 
 for GCM_NAME in $GCM_NAMES_LST; do
+  GCM_NAME_NOSPACE="$(echo -e "${GCM_NAME}" | tr -d '[:space:]')"
   echo -e "\n$GCM_NAME"
   echo "$RCP"
   for i in $rgi_fns 
@@ -46,10 +49,10 @@ for GCM_NAME in $GCM_NAMES_LST; do
     # print the filename
     echo $i
     # determine batch number
-    BATCHNO="$(cut -d'.' -f1 <<<$(cut -d'_' -f5 <<<"$i"))"
+    BATCHNO="$(cut -d'.' -f1 <<<$(cut -d'_' -f6 <<<"$i"))"
     #echo $BATCHNO
     # run the file on a separate node (& tells the command to move to the next loop for any empty nodes)
-    #srun -N 1 -n 1 python run_simulation.py -gcm_name=$GCM_NAME -rcp=$RCP -num_simultaneous_processes=$SLURM_NTASKS_PER_NODE -rgi_glac_number_fn=$i -batch_number=$BATCHNO &
+    srun -N 1 -n 1 python run_simulation.py -gcm_name="$GCM_NAME_NOSPACE" -rcp="$RCP" -num_simultaneous_processes=$SLURM_NTASKS_PER_NODE -rgi_glac_number_fn=$i -batch_number=$BATCHNO &
   done
   # wait tells the loop to not move on until all the srun commands are completed
   wait
