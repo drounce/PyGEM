6c8d8f81403fd664dd764e9577e8b9fab9610d4d
drounce <david.r.rounce@gmail.com>
2023-06-09 11:47:19 -0400

    updating version and setup

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 import setuptools
+from setuptools import find_packages  # or find_namespace_packages
+
 if __name__ == "__main__":
-    setuptools.setup()
+    setuptools.setup(
+        # ...
+        packages=find_packages(
+            # All keyword arguments below are optional:
+            where='pygem',  # '.' by default
+            # include=['mypackage*'],  # ['*'] by default
+            # exclude=['mypackage.tests'],  # empty by default
+        ),
+        # ...
+    )
