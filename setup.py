da271f39c8dfaa2232b01e66469d9243ae991dcc
drounce <david.r.rounce@gmail.com>
2023-06-09 13:01:01 -0400

    modifying setup files

--- setup.py
+++ setup.py
@@ -4,12 +4,16 @@ from setuptools import find_packages  # or find_namespace_packages
 
 if __name__ == "__main__":
     setuptools.setup(
+        # ...,
+        # package_dir={'':'pygem'}
         # ...
-        packages=find_packages(
+        # packages= []
+        # find_packages(
             # All keyword arguments below are optional:
-            where='pygem',  # '.' by default
+            
+            # where='pygem',  # '.' by default
             # include=['mypackage*'],  # ['*'] by default
             # exclude=['mypackage.tests'],  # empty by default
-        ),
+        # ),
         # ...
     )
