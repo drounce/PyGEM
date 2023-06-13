085950915b08d5e55d2ce9aa5658335d9c71b6b5
drounce <david.r.rounce@gmail.com>
2023-06-12 20:59:32 -0400

    switch to sphinx_book_theme

--- docs/conf.py
+++ docs/conf.py
@@ -28,5 +28,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
+
+html_theme_options = {
+    "repository_url": "https://github.com/drounce/PyGEM",
+    "use_repository_button": True,
+#    "use_issues_button": True,
+#    "use_edit_page_button": True,
+#    "path_to_docs": "docs",
+#    # "home_page_in_toc": True,
+#    "toc_title": 'On this page',
+    }
