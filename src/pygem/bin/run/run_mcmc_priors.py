9cdc40d65d61bd37e6055acfa12c16b70d5d5f74
btobers <tobers.brandon@gmail.com>
2024-12-24 11:19:57 -0500

    run_mcmc_priors in progress

--- src/pygem/bin/run/run_mcmc_priors.py
+++ src/pygem/bin/run/run_mcmc_priors.py
@@ -39,7 +39,10 @@ reg_dict = {1:'Alaska',
             17:'S Andes',
             18:'New Zealand',
             19:'Antarctica'}
-
+# list of prior fields
+priors_cn = ['O1Region', 'O2Region', 'count',
+                'kp_mean', 'kp_std', 'kp_med', 'kp_min', 'kp_max', 'kp_alpha', 'kp_beta', 
+                'tbias_mean', 'tbias_std', 'tbias_med', 'tbias_min', 'tbias_max']
 # FUNCTIONS
 def getparser():
     """
@@ -51,21 +54,46 @@ def getparser():
                         help='Randoph Glacier Inventory region (can take multiple, e.g. `-run_region01 1 2 3`)', nargs='+')
     parser.add_argument('-ncores', action='store', type=int, default=1,
                         help='number of simultaneous processes (cores) to use')
+    parser.add_argument('-option_calibration', action='store', type=str, default='emulator',
+                        help='calibration option (defaultss to "emulator")')
+    parser.add_argument('-priors_reg_outpath', action='store', type=str, default=pygem_prms['root'] + '/Output/calibration/' + pygem_prms['calib']['priors_reg_fn'],
+                        help='output path')
     # flags
     parser.add_argument('-v', '--debug', action='store_true',
                         help='Flag for debugging')
+    parser.add_argument('-p', '--plot', action='store_true',
+                        help='Flag for plotting regional priors')
     return parser
 
 
-def run(reg, debug=False):
+def export_priors(priors_df_single, reg, regO2, priors_reg_outpath=''):
+    # EXPORT PRIORS
+    if os.path.exists(priors_reg_outpath):
+        priors_df = pd.read_csv(priors_reg_outpath)
+        # Add or overwrite existing priors
+        priors_idx = np.where((priors_df.O1Region == reg) & (priors_df.O2Region == regO2))[0]
+        if len(priors_idx) > 0:
+            priors_df.loc[priors_idx,:] = priors_df_single.values
+        else:
+            priors_df = pd.concat([priors_df, priors_df_single], axis=0)
+            
+    else:
+        priors_df = priors_df_single
+        
+    priors_df = priors_df.sort_values(['O1Region', 'O2Region'], ascending=[True, True])
+    priors_df.reset_index(inplace=True, drop=True)
+    priors_df.to_csv(priors_reg_outpath, index=False)
+    return priors_df
+
+
+def run(reg, option_calibration='emulator', priors_reg_outpath='', debug=False, plot=False):
 
     # Calibration filepath
-    modelprms_fp = pygem_prms.output_filepath + 'calibration/' + str(reg).zfill(2) + '/'
-    
+    modelprms_fp = pygem_prms['root'] + '/Output/calibration/' + str(reg).zfill(2) + '/'
     # Load glaciers
     glac_list = [x.split('-')[0] for x in os.listdir(modelprms_fp) if x.endswith('-modelprms_dict.json')]
     glac_list = sorted(glac_list)
-
+    
     main_glac_rgi = modelsetup.selectglaciersrgitable(glac_no=glac_list)
     
     # Add model parameters to main dataframe
@@ -78,7 +106,6 @@ def run(reg, debug=False):
     main_glac_rgi['ddfsnow_em'] = np.nan
     main_glac_rgi['mb_mwea_em'] = np.nan
     for nglac, rgino_str in enumerate(list(main_glac_rgi.rgino_str.values)):
-#    for nglac, rgino_str in enumerate(list(main_glac_rgi.rgino_str.values)[0:1]):
         
         glac_str = str(int(rgino_str.split('.')[0])) + '.' + rgino_str.split('.')[1]
         
@@ -86,51 +113,31 @@ def run(reg, debug=False):
         modelprms_fn = glac_str + '-modelprms_dict.json'
         with open(modelprms_fp + modelprms_fn, 'r') as f:
             modelprms_dict = json.load(f)
-        modelprms = modelprms_dict[pygem_prms.option_calibration]
-        
-##        modelprms_fp_em = pygem_prms.output_filepath + 'calibration/' + str(reg).zfill(2) + '-emulator/'
-#        modelprms_fp_em = '/Users/drounce/Documents/HiMAT/spc_backup/calibration/' + str(reg).zfill(2) + '-emulator/'
-#        with open(modelprms_fp_em + modelprms_fn, 'rb') as f:
-#            modelprms_dict_em = pickle.load(f)
-#            modelprms_em = modelprms_dict_em['emulator']
-        
+        assert option_calibration in list(modelprms_dict.keys()), f'{glac_str}: {option_calibration} not in calibration data.'
+        modelprms = modelprms_dict[option_calibration]    
         
         main_glac_rgi.loc[nglac, 'kp'] = modelprms['kp'][0]
         main_glac_rgi.loc[nglac, 'tbias'] = modelprms['tbias'][0]
         main_glac_rgi.loc[nglac, 'ddfsnow'] = modelprms['ddfsnow'][0]
         main_glac_rgi.loc[nglac, 'mb_mwea'] = modelprms['mb_mwea'][0]
-        
-#        main_glac_rgi.loc[nglac, 'kp_em'] = modelprms_em['kp'][0]
-#        main_glac_rgi.loc[nglac, 'tbias_em'] = modelprms_em['tbias'][0]
-#        main_glac_rgi.loc[nglac, 'ddfsnow_em'] = modelprms_em['ddfsnow'][0]
-#        main_glac_rgi.loc[nglac, 'mb_mwea_em'] = modelprms_em['mb_mwea'][0]
-        
         main_glac_rgi.loc[nglac, 'mb_obs_mwea'] = modelprms['mb_obs_mwea'][0]
-        
+    
+    # get regional difference between calibrated mb_mwea and observed
     main_glac_rgi['mb_dif_obs_cal'] = main_glac_rgi['mb_obs_mwea'] - main_glac_rgi['mb_mwea']
-#    main_glac_rgi['mb_dif_obs_em'] = main_glac_rgi['mb_obs_mwea'] - main_glac_rgi['mb_mwea_em']
-       
+
+    # define figure output path
+    if plot:
+        fig_fp = os.path.split(priors_reg_outpath)[0] + '/figs/'
+        os.makedirs(fig_fp, exist_ok=True)
+    
     # Priors for each subregion
     if reg not in [19]:
         rgi_regionsO2 = np.unique(main_glac_rgi.O2Region.values)
         for regO2 in rgi_regionsO2:
             main_glac_rgi_subset = main_glac_rgi.loc[main_glac_rgi['O2Region'] == regO2, :]
+            if plot:
+                plot_hist(main_glac_rgi_subset, fig_fp, reg, regO2)
         
-            # Histograms and record model parameter statistics
-            fig, ax = plt.subplots(1,2, figsize=(6,4), gridspec_kw = {'wspace':0.3, 'hspace':0.3})
-            
-            labelsize = 12
-            
-            fig.text(0.5,0.9, 'Region ' + str(reg) + ' (subregion: ' + str(regO2) + ')', ha='center', size=14)
-            
-            nbins = 50
-            ax[0].hist(main_glac_rgi_subset['kp'], bins=nbins, color='grey')
-            ax[0].set_xlabel('kp (-)')
-            ax[0].set_ylabel('Count (glaciers)')
-            
-            ax[1].hist(main_glac_rgi_subset['tbias'], bins=50, color='grey')
-            ax[1].set_xlabel('tbias (degC)')
-            
             # Precipitation factors
             kp_mean = np.mean(main_glac_rgi_subset['kp'])
             kp_std = np.std(main_glac_rgi_subset['kp'])
@@ -145,11 +152,6 @@ def run(reg, debug=False):
             kp_beta = kp_mean / kp_std
             kp_alpha = kp_mean * kp_beta
             
-            print('\n', reg, '(' + str(regO2) + ')')
-            print('kp (mean/std/med/min/max):', np.round(kp_mean,2), np.round(kp_std,2),
-                  np.round(kp_med,2), np.round(kp_min,2), np.round(kp_max,2))
-            print('  alpha/beta:', np.round(kp_alpha,2), np.round(kp_beta,2))
-            
             # Temperature bias
             tbias_mean = main_glac_rgi_subset['tbias'].mean()
             tbias_std = main_glac_rgi_subset['tbias'].std()
@@ -161,61 +163,28 @@ def run(reg, debug=False):
             if tbias_std == 0:
                 tbias_std = 1
             
-            print('tbias (mean/std/med/min/max):', np.round(tbias_mean,2), np.round(tbias_std,2),
-                  np.round(tbias_med,2), np.round(tbias_min,2), np.round(tbias_max,2))
-        
-            # Save figure
-            fig_fp = pygem_prms.output_filepath + 'calibration/figs/'
-            if not os.path.exists(fig_fp):
-                os.mkdir(fig_fp)
-            fig_fn = str(reg) + '-' + str(regO2) + '_hist_mcmc_priors.png'
-            fig.savefig(fig_fp + fig_fn, pad_inches=0, dpi=150)
-                    
-            # EXPORT PRIORS
-            priors_cn = ['O1Region', 'O2Region', 'count',
-                         'kp_mean', 'kp_std', 'kp_med', 'kp_min', 'kp_max', 'kp_alpha', 'kp_beta', 
-                         'tbias_mean', 'tbias_std', 'tbias_med', 'tbias_min', 'tbias_max']
+            if debug:
+                print('\n', reg, '(' + str(regO2) + ')')
+                print('kp (mean/std/med/min/max):', np.round(kp_mean,2), np.round(kp_std,2),
+                    np.round(kp_med,2), np.round(kp_min,2), np.round(kp_max,2))
+                print('  alpha/beta:', np.round(kp_alpha,2), np.round(kp_beta,2))
+                print('tbias (mean/std/med/min/max):', np.round(tbias_mean,2), np.round(tbias_std,2),
+                    np.round(tbias_med,2), np.round(tbias_min,2), np.round(tbias_max,2))
+
+            # export results
             priors_df_single = pd.DataFrame(np.zeros((1,len(priors_cn))), columns=priors_cn)
             priors_df_single.loc[0,:] = (
                     [reg, regO2, main_glac_rgi_subset.shape[0],
-                     kp_mean, kp_std, kp_med, kp_min, kp_max, kp_alpha, kp_beta, 
-                     tbias_mean, tbias_std, tbias_med, tbias_min, tbias_max])
-            if os.path.exists(pygem_prms.priors_reg_fullfn):
-                priors_df = pd.read_csv(pygem_prms.priors_reg_fullfn)
-                
-                # Add or overwrite existing priors
-                priors_idx = np.where((priors_df.O1Region == reg) & (priors_df.O2Region == regO2))[0]
-                if len(priors_idx) > 0:
-                    priors_df.loc[priors_idx,:] = priors_df_single.values
-                else:
-                    priors_df = pd.concat([priors_df, priors_df_single], axis=0)
-                    
-            else:
-                priors_df = priors_df_single
-                
-            priors_df = priors_df.sort_values(['O1Region', 'O2Region'], ascending=[True, True])
-            priors_df.reset_index(inplace=True, drop=True)
-            priors_df.to_csv(pygem_prms.priors_reg_fullfn, index=False)
+                        kp_mean, kp_std, kp_med, kp_min, kp_max, kp_alpha, kp_beta, 
+                        tbias_mean, tbias_std, tbias_med, tbias_min, tbias_max])
+            priors_df = export_priors(priors_df_single, reg, regO2, priors_reg_outpath)
+    
     # Use the entire region for the prior (sometimes subregions make no sense; e.g., 24 regions in Antarctica)
     else:
         rgi_regionsO2 = np.unique(main_glac_rgi.O2Region.values)
         main_glac_rgi_subset = main_glac_rgi.copy()
-        
-        # Histograms and record model parameter statistics
-        fig, ax = plt.subplots(1,2, figsize=(6,4), gridspec_kw = {'wspace':0.3, 'hspace':0.3})
-        
-        labelsize = 12
-        
-        fig.text(0.5,0.9, 'Region ' + str(reg) + ' (all subregions)', ha='center', size=14)
-        
-        nbins = 50
-        ax[0].hist(main_glac_rgi_subset['kp'], bins=nbins, color='grey')
-        ax[0].set_xlabel('kp (-)')
-        ax[0].set_ylabel('Count (glaciers)')
-        
-        ax[1].hist(main_glac_rgi_subset['tbias'], bins=50, color='grey')
-        ax[1].set_xlabel('tbias (degC)')
-        
+        if plot:
+            plot_hist(main_glac_rgi_subset, fig_fp, reg)
         # Precipitation factors
         kp_mean = np.mean(main_glac_rgi_subset['kp'])
         kp_std = np.std(main_glac_rgi_subset['kp'])
@@ -230,11 +199,6 @@ def run(reg, debug=False):
         kp_beta = kp_mean / kp_std
         kp_alpha = kp_mean * kp_beta
         
-        print('\n', reg, '(all subregions)')
-        print('kp (mean/std/med/min/max):', np.round(kp_mean,2), np.round(kp_std,2),
-              np.round(kp_med,2), np.round(kp_min,2), np.round(kp_max,2))
-        print('  alpha/beta:', np.round(kp_alpha,2), np.round(kp_beta,2))
-        
         # Temperature bias
         tbias_mean = main_glac_rgi_subset['tbias'].mean()
         tbias_std = main_glac_rgi_subset['tbias'].std()
@@ -246,48 +210,44 @@ def run(reg, debug=False):
         if tbias_std == 0:
             tbias_std = 1
         
-        print('tbias (mean/std/med/min/max):', np.round(tbias_mean,2), np.round(tbias_std,2),
-              np.round(tbias_med,2), np.round(tbias_min,2), np.round(tbias_max,2))
-    
-        # Save figure
-        fig_fp = pygem_prms.output_filepath + 'calibration/figs/'
-        if not os.path.exists(fig_fp):
-            os.mkdir(fig_fp)
-        fig_fn = str(reg) + '_hist_mcmc_priors.png'
-        fig.savefig(fig_fp + fig_fn, pad_inches=0, dpi=150)
-        
+        if debug:
+            print('\n', reg, '(all subregions)')
+            print('kp (mean/std/med/min/max):', np.round(kp_mean,2), np.round(kp_std,2),
+                np.round(kp_med,2), np.round(kp_min,2), np.round(kp_max,2))
+            print('  alpha/beta:', np.round(kp_alpha,2), np.round(kp_beta,2))
+            print('tbias (mean/std/med/min/max):', np.round(tbias_mean,2), np.round(tbias_std,2),
+                np.round(tbias_med,2), np.round(tbias_min,2), np.round(tbias_max,2))
         
         for regO2 in rgi_regionsO2:    
-            # EXPORT PRIORS
-            priors_cn = ['O1Region', 'O2Region', 'count',
-                         'kp_mean', 'kp_std', 'kp_med', 'kp_min', 'kp_max', 'kp_alpha', 'kp_beta', 
-                         'tbias_mean', 'tbias_std', 'tbias_med', 'tbias_min', 'tbias_max']
+            # export results
             priors_df_single = pd.DataFrame(np.zeros((1,len(priors_cn))), columns=priors_cn)
             priors_df_single.loc[0,:] = (
                     [reg, regO2, main_glac_rgi_subset.shape[0],
-                     kp_mean, kp_std, kp_med, kp_min, kp_max, kp_alpha, kp_beta, 
-                     tbias_mean, tbias_std, tbias_med, tbias_min, tbias_max])
-            if os.path.exists(pygem_prms.priors_reg_fullfn):
-                priors_df = pd.read_csv(pygem_prms.priors_reg_fullfn)
-                
-                # Add or overwrite existing priors
-                priors_idx = np.where((priors_df.O1Region == reg) & (priors_df.O2Region == regO2))[0]
-                if len(priors_idx) > 0:
-                    priors_df.loc[priors_idx,:] = priors_df_single.values
-                else:
-                    priors_df = pd.concat([priors_df, priors_df_single], axis=0)
-                    
-            else:
-                priors_df = priors_df_single
-                
-            priors_df = priors_df.sort_values(['O1Region', 'O2Region'], ascending=[True, True])
-            priors_df.reset_index(inplace=True, drop=True)
-            priors_df.to_csv(pygem_prms.priors_reg_fullfn, index=False)
-
+                        kp_mean, kp_std, kp_med, kp_min, kp_max, kp_alpha, kp_beta, 
+                        tbias_mean, tbias_std, tbias_med, tbias_min, tbias_max])
+            priors_df = export_priors(priors_df_single, reg, regO2, priors_reg_outpath)
     
-    def plot_priors():
-        pass
+    if plot:
+        plot_reg_priors(priors_df, fig_fp, reg)
+    
+    def plot_hist(main_glac_rgi_subset, fig_fp, reg, regO2=''):
+            # Histograms and record model parameter statistics
+            fig, ax = plt.subplots(1,2, figsize=(6,4), gridspec_kw = {'wspace':0.3, 'hspace':0.3})
+            labelsize = 1
+            fig.text(0.5,0.9, 'Region ' + str(reg) + ' (subregion: ' + str(regO2) + ')'.replace(' (subregion: )', '(all subregions)'), ha='center', size=14)
+            
+            nbins = 50
+            ax[0].hist(main_glac_rgi_subset['kp'], bins=nbins, color='grey')
+            ax[0].set_xlabel('kp (-)')
+            ax[0].set_ylabel('Count (glaciers)')
+            ax[1].hist(main_glac_rgi_subset['tbias'], bins=50, color='grey')
+            ax[1].set_xlabel('tbias (degC)')
+            
+            fig_fn = str(reg) + '-' + str(regO2) + '_hist_mcmc_priors.png'.replace('-_','_')
+            fig.savefig(fig_fp + fig_fn, pad_inches=0, dpi=150)
 
+
+    def plot_reg_priors(priors_df, fig_fp, reg):
         # ===== REGIONAL PRIOR: PRECIPITATION FACTOR ======
         nbins = 50    
         ncols = 3
@@ -295,7 +255,6 @@ def run(reg, debug=False):
         priors_df_regO1 = priors_df.loc[priors_df['O1Region'] == reg]
         
         fig, ax = plt.subplots(nrows, ncols, squeeze=False, gridspec_kw={'wspace':0.5, 'hspace':0.5})
-        
         nrow = 0
         ncol = 0
         for nreg, regO2 in enumerate(rgi_regionsO2):
@@ -339,17 +298,12 @@ def run(reg, debug=False):
                 
         # Labels
         fig.text(0.04, 0.5, 'Probability Density', va='center', ha='center', rotation='vertical', size=12)
-        fig.text(0.5, 0.04, '$k_{p}$ (-)', va='center', ha='center', size=12)
-            
-        # Save figure
-        if os.path.exists(fig_fp) == False:
-            os.makedirs(fig_fp)    
+        fig.text(0.5, 0.04, '$k_{p}$ (-)', va='center', ha='center', size=12)  
         fig.set_size_inches(6, 6)
         fig.savefig(fig_fp + 'priors_kp_O2Regions-' + str(reg) + '.png', bbox_inches='tight', dpi=300)
 
         # ===== REGIONAL PRIOR: TEMPERATURE BIAS ======
-        fig, ax = plt.subplots(nrows, ncols, squeeze=False, gridspec_kw={'wspace':0.3, 'hspace':0.3})
-        
+        fig, ax = plt.subplots(nrows, ncols, squeeze=False, gridspec_kw={'wspace':0.3, 'hspace':0.3})    
         nrow = 0
         ncol = 0
         for nreg, regO2 in enumerate(rgi_regionsO2):
@@ -395,10 +349,6 @@ def run(reg, debug=False):
         # Labels
         fig.text(0.04, 0.5, 'Probability Density', va='center', ha='center', rotation='vertical', size=12)
         fig.text(0.5, 0.04, '$T_{bias}$ ($^\circ$C)', va='center', ha='center', size=12)
-            
-        # Save figure
-        if os.path.exists(fig_fp) == False:
-            os.makedirs(fig_fp)    
         fig.set_size_inches(6, 6)
         fig.savefig(fig_fp + 'priors_tbias_O2Regions-' + str(reg) + '.png', bbox_inches='tight', dpi=300)
     
@@ -416,7 +366,7 @@ def main():
     # Parallel processing
     print('Processing with ' + str(args.ncores) + ' cores...')
     with multiprocessing.Pool(args.ncores) as p:
-        p.map(partial(run,debug=args.debug),args.rgi_region01)
+        p.map(partial(run, option_calibration=args.option_calibration, priors_reg_outpath=args.priors_reg_outpath, debug=args.debug, plot=args.plot),args.rgi_region01)
 
     print('\n\n------\nTotal processing time:', time.time()-time_start, 's')
 
