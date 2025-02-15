19f4d58c1ab7225b068258d147ff601827c56111
Davor Dundovic <33790330+ddundo@users.noreply.github.com>
2025-02-15 19:38:47 +0000

    Format file

--- pygem/tests/test_notebooks.py
+++ pygem/tests/test_notebooks.py
@@ -4,12 +4,17 @@ import subprocess
 import pytest
 
 # Get all notebooks in the PyGEM-notebooks repository
-nb_dir = os.environ["GITHUB_NOTEBOOKS_PATH"] or os.path.join(os.path.expanduser("~"), "PyGEM-notebooks")
+nb_dir = os.environ["GITHUB_NOTEBOOKS_PATH"] or os.path.join(
+    os.path.expanduser("~"), "PyGEM-notebooks"
+)
 notebooks = [f for f in os.listdir(nb_dir) if f.endswith(".ipynb")]
+
 
 @pytest.mark.parametrize("notebook", notebooks)
 def test_notebook(notebook):
     # TODO #54: Test all notebooks
     if notebook not in ("simple_test.ipynb", "advanced_test.ipynb"):
         pytest.skip()
-    subprocess.check_call(["pytest", "--nbmake", os.path.join(nb_dir, notebook)])
+    subprocess.check_call(
+        ["pytest", "--nbmake", os.path.join(nb_dir, notebook)]
+    )
